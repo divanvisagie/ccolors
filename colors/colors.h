bebb3c4ca768c889f90f1820639a62ac0f1a8f98
divanvisagie <visagiedivan@gmail.com>
2013-02-18 05:34:41 +0200

    Started something

--- colors/colors.h
+++ colors/colors.h
@@ -1,4 +1,28 @@
 #ifndef CCOLORS_H
 #define CCOLORS_H
 
+#include <stdio.h>
+
+
+#define CC_CONSOLE_COLOR_DEFAULT "\033[0m"
+#define CC_FORECOLOR(C) "\033[" #C "m"
+#define CC_BACKCOLOR(C) "\033[" #C "m"
+#define CC_ATTR(A) "\033[" #A "m"
+
+typedef struct{
+
+	const char* black;
+	const char* white;
+
+} col;
+
+char* ccolor(){
+
+
+
+	return sprintf(command, "%c[%d;%d;%dm", 0x1B, CC_ATTR(4), 35/*magenta*/ + 30, 30/*black*/ + 40);
+}
+
+
+
 #endif CCOLORS_H
--- test.c
+++ test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#include "colors/colors.h"
+
+
+
 int main( int argc, char** argv ){
+
+	printf("%s\n","hello world");
 
 	return 0;
 }
